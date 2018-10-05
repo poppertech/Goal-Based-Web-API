6eb36c2b89739ee5ff11d5c7678d56c0ead129c1
bwynne <bill.wynne@heidmar.com>
2018-10-05 15:35:24 -0400

    Added File Deserializer Tests

--- Goal-Based-Web-API/ApiTests/Helper.cs
+++ Goal-Based-Web-API/ApiTests/Helper.cs
@@ -1,5 +1,8 @@
 ﻿using Api.Models.Network;
 using CsvHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,6 +25,35 @@ namespace ApiTests
                         return csv.GetRecords<CashFlow>().ToList();
                     }
                 }
+            }
+        }
+
+        public static IFormFile WriteFile(IList<CashFlow> cashFlows)
+        {
+            using (var stream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(stream))
+            using (var csv = new CsvWriter(streamWriter))
+            {
+                csv.WriteRecords(cashFlows);
+                streamWriter.Flush();
+                var bytes = stream.ToArray();
+                var outputStream = new MemoryStream(bytes);
+                return new FormFile(outputStream, 0, outputStream.Length, "CashFlows", "cashflow.csv");
+            }
+        }
+
+        public static IFormFile WriteFile(Node node)
+        {
+            using (var stream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(stream))
+            using (var jsonWriter = new JsonTextWriter(streamWriter))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(jsonWriter, node);
+                streamWriter.Flush();
+                var bytes = stream.ToArray();
+                var outputStream = new MemoryStream(bytes);
+                return new FormFile(outputStream, 0, outputStream.Length, "CashFlows", "cashflow.csv");
             }
         }
 
---
+++ Goal-Based-Web-API/ApiTests/Logic/FileDeserializerTest.cs
@@ -0,0 +1,58 @@
+﻿using Api.Logic;
+using Api.Models.Network;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiTests.Logic
+{
+    [TestClass]
+    public class FileDeserializerTest
+    {
+        [TestMethod]
+        public void CsvFileDeserializerOnSuccessReturnsRecords()
+        {
+            //arrange
+            var cashFlow = new CashFlow { Id = 1, Cost = 100 };
+            var cashFlows = new[] { cashFlow };
+            var file = Helper.WriteFile(cashFlows);
+
+            var csvDeserializer = new CsvFileDeserializer<CashFlow>();
+
+            //act
+            csvDeserializer.ReadFile(file);
+            var records = csvDeserializer.GetRecords();
+
+            //assert
+            Assert.AreEqual(cashFlow.Id, records[0].Id);
+            Assert.AreEqual(cashFlow.Cost, records[0].Cost);
+        }
+
+        [TestMethod]
+        public void JsonFileDeserializerOnSuccessReturnsInstance()
+        {
+            //arrange
+            var distribution = new Distribution(1, 2, 3, 4, 5, 6);
+            var node = new Node { Id = 1, InitialPrice = 2, InitialInvestment = 3, IsPortfolioComponent = true, Distributions = new[] { distribution } };
+            var file = Helper.WriteFile(node);
+
+            var jsonDeserializer = new JsonFileDeserializer<Node>();
+
+            //act
+            jsonDeserializer.ReadFile(file);
+            var instance = jsonDeserializer.GetInstance();
+
+            //assert
+            Assert.AreEqual(node.Id, instance.Id);
+            Assert.AreEqual(node.InitialPrice, instance.InitialPrice);
+            Assert.AreEqual(node.InitialInvestment, instance.InitialInvestment);
+            Assert.IsTrue(instance.IsPortfolioComponent);
+
+            Assert.AreEqual(distribution.Id, instance.Distributions[0].Id);
+            Assert.AreEqual(distribution.Minimum, instance.Distributions[0].Minimum);
+            Assert.AreEqual(distribution.Worst, instance.Distributions[0].Worst);
+            Assert.AreEqual(distribution.Likely, instance.Distributions[0].Likely);
+            Assert.AreEqual(distribution.Best, instance.Distributions[0].Best);
+            Assert.AreEqual(distribution.Maximum, instance.Distributions[0].Maximum);
+        }
+
+    }
+}
